18409bf4f6f7b9c0d1ad1c0bbf6e15004ef970f9
Abiyyu Yafi <yafiabiyyu@protonmail.com>
2023-11-16 05:34:53 +0700

    add company & Job

--- src/icp_rust_boilerplate_backend/src/lib.rs
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -4,8 +4,7 @@ use candid::{Decode, Encode};
 use ic_cdk::api::time;
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
-use std::fmt::format;
-use std::{borrow::Cow, cell::RefCell, collections::HashMap};
+use std::{borrow::Cow, cell::RefCell};
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 type IdCell = Cell<u64, Memory>;
@@ -40,6 +39,32 @@ struct WorkHistory {
     update_at: Option<u64>,
 }
 
+#[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
+struct Company {
+    id:u64,
+    name:String,
+    email:String,
+    phone:String,
+    address:String,
+    description:String,
+    admin: String,
+    created_at:u64
+}
+
+#[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
+struct Job {
+    id: u64,
+    company_id: u64,
+    position: String,
+    requirements:String,
+    location:String,
+    salary: String,
+    description:String,
+    posted_by: String,
+    created_at:u64,
+    updated_at:Option<u64>
+}
+
 // Payload
 #[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
 struct EducationHistoryPayload {
@@ -58,6 +83,24 @@ struct WorkHistoryPayload {
     end_year: Option<u64>,
     salary: u64,
     description: String
+}
+
+#[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
+struct CompanyPayload {
+    name:String,
+    email:String,
+    phone:String,
+    address:String,
+    description:String
+}
+
+#[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
+struct JobPayload {
+    position: String,
+    requirements:String,
+    location:String,
+    salary: String,
+    description:String,
 }
 
 impl Storable for EducationHistory {
@@ -90,6 +133,36 @@ impl BoundedStorable for WorkHistory {
     const IS_FIXED_SIZE: bool = false;
 }
 
+impl Storable for Company {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl BoundedStorable for Company {
+    const MAX_SIZE: u32 = 2048;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+impl Storable for Job {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl BoundedStorable for Job {
+    const MAX_SIZE: u32 = 2048;
+    const IS_FIXED_SIZE: bool = false;
+}
+
 thread_local! {
     static EDUCATION_MEMORY: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
         MemoryManager::init(DefaultMemoryImpl::default())
@@ -102,7 +175,7 @@ thread_local! {
 
     static EDUCATION_STORAGE: RefCell<StableBTreeMap<u64, EducationHistory, Memory>> = RefCell::new(
         StableBTreeMap::init(
-            EDUCATION_MEMORY.with(|m| m.borrow().get(MemoryId::new(0)))
+            EDUCATION_MEMORY.with(|m| m.borrow().get(MemoryId::new(1)))
         )
     );
 
@@ -111,13 +184,43 @@ thread_local! {
     );
 
     static WORK_COUNTER: RefCell<IdCell> = RefCell::new(
-        IdCell::init(WORK_MEMORY.with(|m| m.borrow().get(MemoryId::new(0))), 0)
+        IdCell::init(WORK_MEMORY.with(|m| m.borrow().get(MemoryId::new(2))), 0)
         .expect("Connot create a counter")
     );
 
     static WORK_STORAGE: RefCell<StableBTreeMap<u64, WorkHistory, Memory>> = RefCell::new(
         StableBTreeMap::init(
-            WORK_MEMORY.with(|m| m.borrow().get(MemoryId::new(1)))
+            WORK_MEMORY.with(|m| m.borrow().get(MemoryId::new(3)))
+        )
+    );
+
+    static COMPANY_MEMORY: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
+        MemoryManager::init(DefaultMemoryImpl::default())
+    );
+
+    static COMPANY_COUNTER: RefCell<IdCell> = RefCell::new(
+        IdCell::init(COMPANY_MEMORY.with(|m| m.borrow().get(MemoryId::new(4))), 0)
+        .expect("Connot create a counter")
+    );
+
+    static COMPANY_STORAGE: RefCell<StableBTreeMap<u64, Company, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            COMPANY_MEMORY.with(|m| m.borrow().get(MemoryId::new(5)))
+        )
+    );
+
+    static JOB_MEMORY: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
+        MemoryManager::init(DefaultMemoryImpl::default())
+    );
+
+    static JOB_COUNTER: RefCell<IdCell> = RefCell::new(
+        IdCell::init(JOB_MEMORY.with(|m| m.borrow().get(MemoryId::new(6))), 0)
+        .expect("Connot create a counter")
+    );
+
+    static JOB_STORAGE: RefCell<StableBTreeMap<u64, Job, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            JOB_MEMORY.with(|m| m.borrow().get(MemoryId::new(7)))
         )
     );
 }
@@ -154,7 +257,7 @@ fn add_education_history(payload: EducationHistoryPayload) -> Result<EducationHi
         create_at: time(),
         update_at: None,
     };
-    save_education(&education);
+    _save_education(&education);
     Ok(education)
 }
 
@@ -170,7 +273,7 @@ fn update_education_history(id: u64, payload: EducationHistoryPayload) -> Result
                 education.start_year = payload.start_year;
                 education.end_year = payload.end_year;
                 education.update_at = Some(time());
-                save_education(&education);
+                _save_education(&education);
                 Ok(education)
             } else {
                 Err(Error::NotAuthorize {
@@ -247,7 +350,7 @@ fn add_work_history(payload: WorkHistoryPayload) -> Result<WorkHistory> {
         created_at: time(),
         update_at: None,
     };
-    save_work(&work);
+    _save_work(&work);
     Ok(work)
 }
 
@@ -263,7 +366,7 @@ fn update_work_history(id: u64, payload: WorkHistoryPayload) -> Result<WorkHisto
                 work.salary = payload.salary;
                 work.description = payload.description;
                 work.update_at = Some(time());
-                save_work(&work);
+                _save_work(&work);
                 Ok(work)
             } else {
                 Err(Error::NotAuthorize {
@@ -298,9 +401,210 @@ fn delete_work_history(id: u64) -> Result<WorkHistory> {
     }
 }
 
+#[ic_cdk::query]
+fn get_all_company() -> Result<Vec<Company>> {
+    let company: Vec<(u64, Company)> = COMPANY_STORAGE.with(|service| service.borrow().iter().collect());
+    let total_company = company.len();
+    let mut comp: Vec<Company> = Vec::new();
+    for key in 0..total_company {
+        comp.push(company.get(key).unwrap().clone().1);
+    }
+    if comp.len() > 0 {
+        Ok(comp)
+    }else {
+        Err(Error::NotFound { msg: format!(" There are currently no company") })
+    }
+}
+
+#[ic_cdk::update]
+fn register_company(payload: CompanyPayload) -> Result<Company> {
+    let id = COMPANY_COUNTER.with(|counter| {
+        let current_value = *counter.borrow().get();
+        counter.borrow_mut().set(current_value + 1)
+    }).expect("Cannot increment counter");
+
+    let company = Company {
+        id:id,
+        name:payload.name,
+        email:payload.email,
+        phone:payload.phone,
+        address: payload.address,
+        description:payload.description,
+        admin:ic_cdk::caller().to_string(),
+        created_at: time()
+    };
+    _save_company(&company);
+    Ok(company)
+}
+
+#[ic_cdk::update]
+fn update_company(id:u64, payload:CompanyPayload) -> Result<Company> {
+    match _get_company(&id) {
+        Some(mut company) => {
+            if company.admin == ic_cdk::caller().to_string() {
+                company.name = payload.name;
+                company.email = payload.email;
+                company.phone = payload.phone;
+                company.address = payload.address;
+                company.description = payload.description;
+                _save_company(&company);
+                Ok(company)
+            }else {
+                Err(Error::NotAuthorize {
+                    msg: format!("{} not company admin", ic_cdk::caller().to_string()),
+                })
+            }
+        },
+        None => {
+            Err(Error::NotFound { msg: format!("Company with id {} not found", id) })
+        }
+    }
+}
+
+#[ic_cdk::update]
+fn delete_company(id:u64) -> Result<Company> {
+    match _get_company(&id) {
+        Some(mut company) => {
+            if company.admin == ic_cdk::caller().to_string() {
+                match COMPANY_STORAGE.with(|service|service.borrow_mut().remove(&id)) {
+                    Some(comp) => Ok(comp),
+                    None => {
+                        Err(Error::NotFound { msg: format!("Company with id {} not found", id) })
+                    }
+                }
+            }else {
+                Err(Error::NotAuthorize {
+                    msg: format!("{} not company admin", ic_cdk::caller().to_string()),
+                })
+            }
+        },
+        None => {
+            Err(Error::NotFound { msg: format!("Company with id {} not found", id) })
+        }
+    }
+}
+
+#[ic_cdk::query]
+fn get_all_job() -> Result<Vec<Job>> {
+    let job: Vec<(u64, Job)> = JOB_STORAGE.with(|service| service.borrow().iter().collect());
+    let total_job = job.len();
+    let mut jobs: Vec<Job> = Vec::new();
+    for key in 0..total_job {
+        jobs.push(job.get(key).unwrap().clone().1);
+    }
+    if jobs.len() > 0 {
+        Ok(jobs)
+    }else {
+        Err(Error::NotFound { msg: format!(" There are currently no job") })
+    }
+}
+
+#[ic_cdk::query]
+fn get_job_by_company(id:u64) -> Result<Vec<Job>> {
+    JOB_STORAGE.with(|job| {
+        Ok(job
+            .borrow()
+            .iter()
+            .filter(|(_, v)| v.company_id == id)
+            .map(|(_, v)| v.clone())
+            .collect()
+        )
+    })
+}
+
+
+#[ic_cdk::update]
+fn post_new_job(companys_id:u64, payload:JobPayload) -> Result<Job> {
+    match _get_company(&companys_id) {
+        Some(mut company) => {
+            if company.admin == ic_cdk::caller().to_string() {
+                let id = JOB_COUNTER.with(|counter| {
+                    let current_value = *counter.borrow().get();
+                    counter.borrow_mut().set(current_value + 1)
+                })
+                .expect("Cannot increment counter");
+
+                let job = Job {
+                    id: id,
+                    company_id: companys_id,
+                    position: payload.position,
+                    requirements: payload.requirements,
+                    location: payload.location,
+                    salary: payload.salary,
+                    description: payload.description,
+                    posted_by: ic_cdk::caller().to_string(),
+                    created_at: time(),
+                    updated_at: None
+                };
+                _save_job_post(&job);
+                Ok(job)
+            }else {
+                Err(Error::NotAuthorize {
+                    msg: format!("{} not owner", ic_cdk::caller().to_string()),
+                })
+            }
+        },
+        None => Err(Error::NotFound {
+            msg: format!("Company with id {} not found", companys_id),
+        }),
+    }
+}
+
+#[ic_cdk::update]
+fn update_job_post(id:u64, payload:JobPayload) -> Result<Job> {
+    match _get_job_post(&id) {
+        Some(mut job) => {
+            if job.posted_by == ic_cdk::caller().to_string() {
+                job.position = payload.position;
+                job.requirements = payload.requirements;
+                job.location = payload.location;
+                job.salary = payload.salary;
+                job.description = payload.description;
+                job.updated_at = Some(time());
+                _save_job_post(&job);
+                Ok(job)
+            }else {
+                Err(Error::NotAuthorize {
+                    msg: format!("{} not owner", ic_cdk::caller().to_string()),
+                })
+            }
+        },
+        None => Err(Error::NotFound {
+            msg: format!("Job with id {} not found", id),
+        }),
+    }
+}
+
+#[ic_cdk::update]
+fn delete_job_post(id:u64) -> Result<Job> {
+    match _get_job_post(&id) {
+        Some(job) => {
+            if job.posted_by == ic_cdk::caller().to_string() {
+                match JOB_STORAGE.with(|service| service.borrow_mut().remove(&id)) {
+                    Some(jobs) => Ok(jobs),
+                    None => {
+                        Err(Error::NotFound {
+                            msg: format!("Job Post with id {} not found", id),
+                        })
+                    }
+                }
+            }else {
+                Err(Error::NotAuthorize {
+                    msg: String::from("Not authorized to delete this job post"),
+                })
+            }
+        },
+        None => {
+            Err(Error::NotFound {
+                msg: format!("Education with id {} not found", id),
+            })
+        }
+    }
+}
+
 // Helper function
 
-fn save_education(data: &EducationHistory) {
+fn _save_education(data: &EducationHistory) {
     EDUCATION_STORAGE.with(|service| service.borrow_mut().insert(data.id, data.clone()));
 }
 
@@ -308,12 +612,28 @@ fn _get_education(id: &u64) -> Option<EducationHistory> {
     EDUCATION_STORAGE.with(|service| service.borrow().get(&id))
 }
 
-fn save_work(data: &WorkHistory) {
+fn _save_work(data: &WorkHistory) {
     WORK_STORAGE.with(|service| service.borrow_mut().insert(data.id, data.clone()));
 }
 
 fn _get_work(id: &u64) -> Option<WorkHistory> {
     WORK_STORAGE.with(|service| service.borrow().get(&id))
+}
+
+fn _save_company(data: &Company) {
+    COMPANY_STORAGE.with(|service| service.borrow_mut().insert(data.id, data.clone()));
+}
+
+fn _get_company(id:&u64) -> Option<Company>{
+    COMPANY_STORAGE.with(|service| service.borrow().get(&id))
+}
+
+fn _save_job_post(data: &Job) {
+    JOB_STORAGE.with(|service| service.borrow_mut().insert(data.id, data.clone()));
+}
+
+fn _get_job_post(id:&u64) -> Option<Job> {
+    JOB_STORAGE.with(|service| service.borrow().get(&id))
 }
 
 #[derive(candid::CandidType, Deserialize, Serialize)]
